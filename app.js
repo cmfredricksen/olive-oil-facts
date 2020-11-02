2cb590dbf0a6cf88a3813ef04c2efb19b4ad0ea8
Chrissy <christine@christinefredricksen.com>
2020-11-01 18:07:09 -0600

    added photo to top of page

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 "use strict";
+const btnsShowModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+console.log(btnsShowModal);
 
-const expand = document.querySelectorAll(".expand");
+const modal = document.querySelector(".modal");
 const hidden = document.querySelector(".hidden");
-console.log(expand);
-for (let i = 0; i < expand.length; i++) {
-  expand[i].addEventListener("click", function () {
-    hidden.classList.remove("hidden");
-    console.log(expand[i]);
-  });
+const overlay = document.querySelector(".overlay");
+
+let openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+let hideModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener("click", openModal);
 }
+
+// btnCloseModal.addEventListener("click", hideModal);
+// overlay.addEventListener("click", hideModal);
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    hideModal();
+  }
+});
